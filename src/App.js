d7b5afb937f521653eaa52dd78bc07c753587eff
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-24 17:11:45 -0800

    Add state, begin adding JSX elements, create Main Form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import React, { Component } from "react"
+import MainForm from "./Components/MainForm"
 
-class App extends { Component } {
+class App extends Component {
   constructor () {
     super()
     this.state = {
-      
+      generalInformation: { name: "", email: "", phone: ""},
+      education: [{name: "", title: "", date: ""}],
+      experience: [{name: "", title: "", tasks: "", dateStart: "", dateEnd: ""}]
     }
   }
 
+  render(){
 
+    const { generalInformation, education, experience } = this.state
+
+
+    return (
+      <main>
+        <MainForm />
+      </main>
+
+    )
+
+
+  }
 
 }
 
---
+++ src/Components/MainForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+
+
+const MainForm = (props) => {
+
+
+
+    return (
+        <div>
+            <form>
+                <input />
+            </form>
+        </div>
+    )
+
+}
+
+
+export default MainForm
