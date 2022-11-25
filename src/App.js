2b235ff92bf1a207292c267b4905472718fe143c
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-24 16:38:24 -0800

    Delete unneccessary files and imports. Add class object to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
 
-const introduce = (user) => {
-  return `${user.firstName} ${user.lastName}`
-}
-const user = {
-  firstName: "Butter",
-  lastName: "Biscuits"
-}
+class App extends { Component } {
+  constructor () {
+    super()
+    this.state = {
+      
+    }
+  }
 
-const name = "Blue Jones"
-function App() {
-  return (
-    <h1>Hello {introduce(user)}</h1>
-  );
+
+
 }
 
 export default App;
